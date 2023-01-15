a91f6fbd49e49a24a8b25fcf804b08c095b59932
Denis Popkov <pkvdenis@gmail.com>
2023-01-15 20:07:35 +0300

    Add model and factory classes to get test results.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,8 +1,11 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("main");
+        List<Flight> flights = FlightBuilder.createFlights();
+        flights.forEach(System.out::println);
     }
 }
